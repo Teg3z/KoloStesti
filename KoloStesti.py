562d543a2459d758350a4d6ed31d6df3c5a95021
Teg3z <tegez@gmail.com>
2022-11-13 00:24:52 +0100

    basic kolo

--- KoloStesti.py
+++ KoloStesti.py
@@ -1,1 +1,7 @@
-print("Kolo stesti")
+
+import random
+
+list_her = ["Apex Legends", "Overwatch", "PUBG: Battlegrounds", "Payday 2", "League of Legends", 
+            "Counter Strike: Global Offensive", "Fortnite", "Programovani kola stesti", "Grant Treft Auto V", "Lost Ark"]
+
+print(random.choice(list_her))
