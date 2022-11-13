ce9a762162c5fb39c0d6ad4be979a44f547da9a8
Teg3z <tegez@gmail.com>
2022-11-13 19:14:36 +0100

    pridani odpoctu

--- KoloStesti.py
+++ KoloStesti.py
@@ -1,7 +1,18 @@
-
+# importy
 import random
+import time
 
-list_her = ["Apex Legends", "Overwatch", "PUBG: Battlegrounds", "Payday 2", "League of Legends", 
+# list her, ktere pripadaji v uvahu
+list_her = ["Apex Legends", "Overwatch", "PUBG: Battlegrounds", "Payday 2", "League of Legends",
             "Counter Strike: Global Offensive", "Fortnite", "Programovani kola stesti", "Grant Treft Auto V", "Lost Ark"]
 
+#odpocet
+for i in range(3,0, -1):
+    print(i)
+    time.sleep(1)
+
+print("Hra pro dnesni den je...")  
+time.sleep(1)
+  
+# vybrani viteze
 print(random.choice(list_her))
