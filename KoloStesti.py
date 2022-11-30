5d4a0b0b70b7fe016bc312db6d6521a4fc13ba6f
Teg3z <tegez@gmail.com>
2022-12-01 00:25:33 +0100

    dkm button add

--- KoloStesti.py
+++ KoloStesti.py
@@ -9,6 +9,7 @@ from Hra import Hra
 # absultni cesta k logum
 CESTA_LOGY_DK = r"REPLACED_PATH\KoloStesti\LogsDK.txt"
 CESTA_LOGY_DFK = r"REPLACED_PATH\KoloStesti\LogsDFK.txt"
+CESTA_LOGY_DKM = r"REPLACED_PATH\KoloStesti\LogsDKM.txt"
 CESTA_LOGY_D = r"REPLACED_PATH\KoloStesti\LogsD.txt"
 CESTA_LOGY_DFKM = r"REPLACED_PATH\KoloStesti\LogsDFKM.txt"
 CESTA_KDO_BYL_TOCEN_NAPOSLED = r"REPLACED_PATH\KoloStesti\KdoBylTocenNaposled.txt"
@@ -58,6 +59,9 @@ def RemoveUnwantedGames(list_her_grafika, list_her, window, kdo_chce_hrat):
         if kdo_chce_hrat in list_her[index].list_chticu:
             window[list_her_grafika[index].key].Update(visible = True)
             wanted_games.append(list_her_grafika[index])
+            if (kdo_chce_hrat == "DKM" and list_her_grafika[index].key == "League of Legends"):
+                for i in range(0,9):
+                    wanted_games.append(list_her_grafika[index])
         else:
             window[list_her_grafika[index].key].Update(visible = False)
 
@@ -103,16 +107,16 @@ def Toceni(list_konkretni, kc):
     return vyherni_hra
 
 # list her, ktere pripadaji v uvahu
-apex = Hra("Apex Legends", ["DK", "D", "K"], 1)
-pubg = Hra("PUBG: Battlegrounds", ["DK", "K"], 1)
-csgo = Hra("Counter Strike: Global Offensive", ["DK", "D"], 1)
-fortnite = Hra("Fortnite", ["DK", "D"], 1)
-programovani = Hra("Programovani kola stesti", ["DK"], 1)
-lost_ark = Hra("Lost Ark", ["DK", "D", "K"], 1)
-payday2 = Hra("Payday 2", ["DFK", "DK", "FK", "F", "K"], 1)
-lolko = Hra("League of Legends", ["DFKM", "DFK", "DK", "FK","DM", "D", "M"], 1)
-fall_guys = Hra("Fall Guys", ["DFK", "DK", "DF", "FK", "D", "K", "F"], 1)
-overwatch = Hra("Overwatch", ["DFK", "DK", "DF", "FK", "D", "K", "F"], 1)
+apex = Hra("Apex Legends", ["DK", "D", "K", "DKM"], 1)
+pubg = Hra("PUBG: Battlegrounds", ["DK", "K", "DKM"], 1)
+csgo = Hra("Counter Strike: Global Offensive", ["DK", "D", "DKM"], 1)
+fortnite = Hra("Fortnite", ["DK", "D", "DKM"], 1)
+programovani = Hra("Programovani kola stesti", ["DK", "DKM"], 1)
+lost_ark = Hra("Lost Ark", ["DK", "D", "K", "DKM"], 1)
+payday2 = Hra("Payday 2", ["DFK", "DK", "FK", "F", "K", "DKM"], 1)
+lolko = Hra("League of Legends", ["DFKM", "DFK", "DK", "FK","DM", "D", "M", "DKM"], 1)
+fall_guys = Hra("Fall Guys", ["DFK", "DK", "DF", "FK", "D", "K", "F", "DKM"], 1)
+overwatch = Hra("Overwatch", ["DFK", "DK", "DF", "FK", "D", "K", "F", "DKM"], 1)
 gta = Hra("Grant Treft Auto V", ["DFK", "F"], 1)
 
 list_her = [apex, pubg, csgo, fortnite, programovani, lost_ark, payday2, lolko, fall_guys, overwatch, gta]
@@ -138,6 +142,7 @@ for hra in list_her:
 # buttony
 dk_button = grafika.Button("DK", button_color = 'Green', font = nas_font , mouseover_colors='DarkGreen', size = (7,0))
 dfk_button = grafika.Button("DFK", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
+dkm_button = grafika.Button("DKM", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
 d_button = grafika.Button("D", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
 dfkm_button = grafika.Button("DFKM", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
 w = grafika.Button("W", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
@@ -152,7 +157,7 @@ for hra in list_her_grafika:
 
 # pridej buttony
 layout.append([output])
-layout.append([dk_button, dfk_button, d_button, dfkm_button])
+layout.append([dk_button, dfk_button, dkm_button, d_button, dfkm_button])
 layout.append([minula_hra])
 layout.append([w,l])
 layout.append([winlose])
@@ -177,6 +182,9 @@ while True:
     elif event == "DFK":
         konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "DFK"), random.uniform(0.3, 0.8))
         koho_jsme_tocili = "DFK"
+    elif event == "DKM":
+        konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "DKM"), random.uniform(0.3, 0.8))
+        koho_jsme_tocili = "DKM"
     elif event == "D":
         konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "D"), random.uniform(0.3, 0.8))
         koho_jsme_tocili = "D"
