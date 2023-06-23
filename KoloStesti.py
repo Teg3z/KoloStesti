68d7ba395d138a1bd61906130049de2f4abc9ca3
Teg3z <tegez623@gmail.com>
2023-06-23 18:46:19 +0200

    MongoDB update, code sexy
    Co-authored-by: R4tmax <R4tmax@users.noreply.github.com>

--- KoloStesti.py
+++ KoloStesti.py
@@ -2,205 +2,142 @@
 import random
 import time
 import PySimpleGUI as grafika
-from datetime import date
 import discord_bot
+from datetime import datetime
 from Hra import Hra
-
-import pprint
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-# absultni cesta k logum
-CESTA_LOGY = r"REPLACED_PATH\KoloStesti\\"
-
-CESTA_LOGY_DK = CESTA_LOGY + "LogsDK.txt"
-CESTA_LOGY_DF = CESTA_LOGY + "LogsDF.txt"
-CESTA_LOGY_DFK = CESTA_LOGY + "LogsDFK.txt"
-CESTA_LOGY_DKM = CESTA_LOGY + "LogsDKM.txt"
-CESTA_LOGY_D = CESTA_LOGY + "LogsD.txt"
-CESTA_LOGY_DFKM = CESTA_LOGY + "LogsDFKM.txt"
-CESTA_KDO_BYL_TOCEN_NAPOSLED = CESTA_LOGY + "KdoBylTocenNaposled.txt"
-CESTA_VYHERNI_HRA = CESTA_LOGY + "VyherniHra.txt"
-CESTA_POSLEDNI_HRA = CESTA_LOGY + "PosledniHra.txt"
-CESTA_LOGY_DKKA = CESTA_LOGY + "LogsDKKA.txt"
-
-
-# uri = "REPLACED_DB_CONNECTION_STRING"
+# pripoj se k DB
+# databaze
+uri = "REPLACED_DB_CONNECTION_STRING"
 # # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-# # Send a ping to confirm a successful connection
-# db = client['WheelOfLuck']
+client = MongoClient(uri, server_api=ServerApi('1'))
+# create database
+db = client['WheelOfLuck']
 
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
+def ZjistiCoJsmeTociliNaposled():
+    collection = db['LastSpin']
+    entry = collection.find_one()
+    return entry['last_game'] 
+    
 
+# zapis posledni toceni do tabulky v MongoDB
+def ZapisDoMongoDBLastSpin(kategorie, posledni_hra):
+    collection = db['LastSpin']
 
-# collection = db['LastSpin']
+    entry = collection.find_one()
 
-# entry = collection.find_one()
+    filter = {'_id': entry['_id']}
 
-# filter = {'_id': entry['_id']}
-# newvalues = { "$set": { "last_game": "Fortnite" } }
+    # When was last game spinned
+    game_time = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
+    
+    newvalues = { "$set": { 
+        "last_category" : kategorie,
+        "last_game": posledni_hra,
+        "last_game_date": game_time
+         } 
+        }
+    
+    collection.update_one(filter, newvalues)
 
-# collection.update_one(filter, newvalues)
+# zapis vysledek posledniho toceni do tabulky v MongoDB
+def ZapisDoMongoDBLog(vysledek):
+    collection = db['LastSpin']
+    entry = collection.find_one()
+    collection = db["Logs" + entry['last_category']]
 
-# #pprint.pprint(entry['last_game'])
+    post = {"game_date": entry['last_game_date'],
+            "game": entry['last_game'],
+            "result": vysledek}
 
+    collection.insert_one(post)
 
-# #post = {"last_category": "DKKA",
-# #        "last_game": "Apex Legends"}
-
-
-# print(str(db.list_collection_names()))
-# #collection.insert_one(post)
-
-
-def ZjistiKohoJsmeTociliNaposled(cesta):
-    try:
-        soubor = open(cesta, "rt")
-        return soubor.read()
-    except:
-        print("Soubor KdoBylTocenNaposled nenelezen nebo poškozen.")
-        exit()
-
-def ZapisKdoBylTocen(koho_jsme_tocili, cesta):
-    try:
-        soubor = open(cesta, "wt")
-        soubor.write(koho_jsme_tocili)
-    except:
-        print("Soubor KdoBylTocenNaposled nenelezen nebo poškozen.")
-        exit()
-
-def NajdiLogy(koho_jsme_tocili):
-    if koho_jsme_tocili == "DK":
-        return CESTA_LOGY_DK
-    elif koho_jsme_tocili == "DFK":
-        return CESTA_LOGY_DFK
-    elif koho_jsme_tocili == "D":
-        return CESTA_LOGY_D
-    elif koho_jsme_tocili == "DFKM":
-        return CESTA_LOGY_DFKM
-    elif koho_jsme_tocili == "DKM":
-        return CESTA_LOGY_DKM
-    elif koho_jsme_tocili == "DF":
-        return CESTA_LOGY_DF
-    elif koho_jsme_tocili == "DKKA":
-        return CESTA_LOGY_DKKA
-
-def ZapisDoDatabaze(koho_jsme_tocili, text):
-    # databaze
-    umisteni_logu = NajdiLogy(koho_jsme_tocili)
-    try:
-        databaze = open(umisteni_logu, "at")
-        databaze.write(text)
-        databaze.close()
-    except:
-        print("Soubor s logy nenalezen nebo poškozen.")
-        exit()
-
-
+# odstran hry podle katerogie, ktera je tocena
 def RemoveUnwantedGames(list_her_grafika, list_her, window, kdo_chce_hrat):
+    wanted_games_grafika = []
     wanted_games = []
-    games = []
 
     for index in range(0,len(list_her_grafika)):
         if kdo_chce_hrat in list_her[index].list_chticu:
             window[list_her_grafika[index].key].Update(visible = True)
-            wanted_games.append(list_her_grafika[index])
-            games.append(list_her[index])
+            wanted_games_grafika.append(list_her_grafika[index])
+            wanted_games.append(list_her[index])
         else:
             window[list_her_grafika[index].key].Update(visible = False)
 
-    window.refresh()
+    window.refresh()  
+    return wanted_games_grafika, wanted_games
 
-    VyberVyherniHru(games)
-    
-    return wanted_games
-
+# Zobraz vsechny hry
 def MakeAllVisible(list_konkretni, window):
     for hra in list_konkretni:
         window[hra.Get()].Update(visible = True)
 
+# Udelej vsechny hry bilou barvou
 def Vybel(list_konkretni):
     for hra_bila in list_konkretni:
         hra_bila.update(text_color='White')
 
+# Vyber hru (class Hra), ktera padne pri toceni
 def VyberVyherniHru(list_konkretni):
     procenta = []
     for hra in list_konkretni:
         procenta.append(hra.procenta)
-    vyherni_hra = random.choices(list(list_konkretni),weights=procenta, k=1)
+    return random.choices(list(list_konkretni),weights=procenta, k=1)
 
-    try:
-        soubor = open(CESTA_VYHERNI_HRA, "wt")
-        soubor.write(vyherni_hra[0].nazev)
-    except:
-        print("nenalezen nebo poškozen.")
-        exit()
-
-
-
-def Toceni(list_konkretni, kc):
-
-    Vybel(list_konkretni)
-
+# graficke toceni kola
+def Toceni(list_her_grafika, list_her):
+    Vybel(list_her_grafika)
+    vyherni_hra_nazev = VyberVyherniHru(list_her)[0].nazev
     interval = 0.01
-    konecny_cas = kc
-    try:
-        soubor = open(CESTA_VYHERNI_HRA, "rt")
-        vyherni_hra_nazev = soubor.read()
-    except:
-        print("nenalezen nebo poškozen.")
-        exit()
+    konecny_cas = random.uniform(0.3, 0.8)
     
-    for hra in list_konkretni:
+    for hra in list_her_grafika:
         if (hra.Get() == vyherni_hra_nazev):
-            vyherni_hra = hra
-    #vyherni_hra = random.choice(list(list_konkretni))
-    hra_bila = list_konkretni[0]
+            vyherni_hra_grafika = hra
+    hra_bila = list_her_grafika[0]
     koncime = False
 
     while not koncime:
-        for hra_zelena in list_konkretni:
+        for hra_zelena in list_her_grafika:
             hra_zelena.update(text_color='Lime')
             if hra_bila.key != hra_zelena.key:
                 hra_bila.update(text_color='White')
             hra_bila = hra_zelena
             window.refresh()
             time.sleep(interval)
-            if interval > konecny_cas and hra_bila == vyherni_hra:
+            if interval > konecny_cas and hra_bila == vyherni_hra_grafika:
                 koncime = True
                 break
 
             interval+=0.02    
 
     # vybrani viteze
-    output.update("\nUžijte si " + vyherni_hra.Get())
+    output.update("\nUžijte si " + vyherni_hra_grafika.Get())
     window.refresh()
 
-    return vyherni_hra
+    return vyherni_hra_grafika
 
 # list her, ktere pripadaji v uvahu
-apex = Hra("Apex Legends", ["DK", "D", "K", "DKM", "DKKA"], 1)
-pubg = Hra("PUBG: Battlegrounds", ["DK", "K", "DKM", "D", "DKKA"], 1)
-csgo = Hra("Counter Strike: Global Offensive", ["DK", "D", "DKM", "DKKA"], 1)
-fortnite = Hra("Fortnite", ["DK", "D", "DKM"],1)
+apex = Hra("Apex Legends", ["DK", "D", "K", "DKKA"], 1)
+pubg = Hra("PUBG: Battlegrounds", ["DK", "K", "D", "DKKA"], 1)
+csgo = Hra("Counter Strike: Global Offensive", ["DK", "D", "DKKA"], 1)
+fortnite = Hra("Fortnite", ["DK", "D"],1)
 programovani = Hra("Programovani kola stesti", ["DK", "D", "DKKA"], 1)
-lost_ark = Hra("Lost Ark", ["DK", "D", "K", "DKM", "DFK"], 1)
-#payday2 = Hra("Payday 2", ["DFK", "DK", "FK", "F", "K", "DKM"], 1)
-lolko = Hra("League of Legends", ["DM", "D", "M", "DKM", "DF"], 1)
-fall_guys = Hra("Fall Guys", ["DFK", "DK", "DF", "FK", "D", "K", "F", "DKM"], 1)
-overwatch = Hra("Overwatch", ["DFK", "DK", "DF", "FK", "D", "K", "F", "DKM", "DKKA"], 1)
+lost_ark = Hra("Lost Ark", ["DK", "D", "K", "DFK"], 1)
+#payday2 = Hra("Payday 2", ["DFK", "DK", "FK", "F", "K"], 1)
+lolko = Hra("League of Legends", ["DM", "D", "M", "DF", "DFKM"], 1)
+fall_guys = Hra("Fall Guys", ["DFK", "DK", "DF", "FK", "D", "K", "F"], 1)
+overwatch = Hra("Overwatch", ["DFK", "DK", "DF", "FK", "D", "K", "F", "DKKA"], 1)
 gta = Hra("Grant Treft Auto V", ["DFK", "F", "DK", "DF"], 1)
 keep_talking = Hra("Keep Talking and Nobody Explodes", ["DK", "DF"], 1)
 orcs = Hra("Orcs Must Die", ["DK", "K"], 1)
 deceive = Hra("Deceive", ["DFK", "DK", "DF"], 1)
+dead_by_daylight = Hra("Dead by Daylight", ["DK", "DKKA"], 1)
 
-list_her = [apex, pubg, csgo, fortnite, programovani, lost_ark, lolko, fall_guys, overwatch, gta, keep_talking, orcs, deceive]
+list_her = [apex, pubg, csgo, fortnite, programovani, lost_ark, lolko, fall_guys, overwatch, gta, keep_talking, orcs, deceive, dead_by_daylight]
 
 # barvy
 back = "Black"
@@ -214,7 +151,7 @@ output = grafika.Text("", text_color=front, background_color=back, font = nas_fo
 minula_hra = grafika.Text("\nJak dopadla minulá hra?", text_color=front, background_color=back, font = nas_font)
 winlose = grafika.Text("", text_color=front, background_color=back, font = nas_font)
 
-posledniHraText = "Naposledy točeno: " + ZjistiKohoJsmeTociliNaposled(CESTA_POSLEDNI_HRA)
+posledniHraText = "Naposledy točeno: " + ZjistiCoJsmeTociliNaposled()
 posledniHra = grafika.Text(posledniHraText, text_color=front, background_color=back, font = nas_font)
 
 list_her_grafika = []
@@ -227,7 +164,6 @@ for hra in list_her:
 dk_button = grafika.Button("DK", button_color = 'Green', font = nas_font , mouseover_colors='DarkGreen', size = (7,0))
 df_button = grafika.Button("DF", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
 dfk_button = grafika.Button("DFK", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
-dkm_button = grafika.Button("DKM", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
 d_button = grafika.Button("D", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
 dfkm_button = grafika.Button("DFKM", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
 dkka_button = grafika.Button("DKKA", button_color = 'Green', font = nas_font, mouseover_colors='DarkGreen', size = (7,0))
@@ -243,7 +179,7 @@ for hra in list_her_grafika:
 
 # pridej buttony
 layout.append([output])
-layout.append([dk_button, dfk_button, dkm_button, d_button, dfkm_button, df_button, dkka_button])
+layout.append([dk_button, dfk_button, d_button, dfkm_button, df_button, dkka_button])
 layout.append([minula_hra])
 layout.append([w,l])
 layout.append([winlose])
@@ -262,41 +198,21 @@ while True:
     # precteni okna
     event, values = window.read()
 
-    # zmacknuti tlacitka ZATOC
-    if event == "DK":
-        konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "DK"), random.uniform(0.3, 0.8))
-        koho_jsme_tocili = "DK"
-    elif event == "DFK":
-        konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "DFK"), random.uniform(0.3, 0.8))
-        koho_jsme_tocili = "DFK"
-    elif event == "DF":
-        konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "DF"), random.uniform(0.3, 0.8))
-        koho_jsme_tocili = "DF"
-    elif event == "DKM":
-        konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "DKM"), random.uniform(0.3, 0.8))
-        koho_jsme_tocili = "DKM"
-    elif event == "D":
-        konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "D"), random.uniform(0.3, 0.8))
-        koho_jsme_tocili = "D"
-    elif event == "DFKM":
-        konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "DFKM"), random.uniform(0.3, 0.8))
-        koho_jsme_tocili = "DFKM"
-    elif event == "DKKA":
-        konecna_vyherni_hra = Toceni(RemoveUnwantedGames(list_her_grafika, list_her , window, "DKKA"), random.uniform(0.3, 0.8))
-        koho_jsme_tocili = "DKKA"
-    elif event == "W":
-        koho_jsme_tocili = ZjistiKohoJsmeTociliNaposled(CESTA_KDO_BYL_TOCEN_NAPOSLED)
-        ZapisDoDatabaze(koho_jsme_tocili, "W\n")
-        winlose.update("\n YOU ARE THE BEST" )
-    elif event == "L":
-        koho_jsme_tocili = ZjistiKohoJsmeTociliNaposled(CESTA_KDO_BYL_TOCEN_NAPOSLED)
-        ZapisDoDatabaze(koho_jsme_tocili, "L\n")
-        winlose.update("\n YOU SUCK" )
+    # zmacknuti W nebo L
+    if event == "W" or event == "L":
+        if event == "W":
+            winlose.update("\n YOU ARE THE BEST" )
+        else:
+            winlose.update("\n YOU SUCK" )
+        ZapisDoMongoDBLog(event)
+    # toceni
+    elif event != grafika.WIN_CLOSED:
+        G, H = RemoveUnwantedGames(list_her_grafika, list_her , window, event)
+        konecna_vyherni_hra = Toceni(G,H)
+        koho_jsme_tocili = event
     # zavreni okna
-    elif event == grafika.WIN_CLOSED:
-        ZapisDoDatabaze(koho_jsme_tocili, date.today().strftime("%d.%m.%Y") + " " + konecna_vyherni_hra.Get() + " ")
-        ZapisKdoBylTocen(koho_jsme_tocili, CESTA_KDO_BYL_TOCEN_NAPOSLED)
-        ZapisKdoBylTocen(konecna_vyherni_hra.Get(), CESTA_POSLEDNI_HRA)
+    else:
+        ZapisDoMongoDBLastSpin(koho_jsme_tocili, konecna_vyherni_hra.Get())
         discord_bot.StartBot(konecna_vyherni_hra.Get())
         break
 
