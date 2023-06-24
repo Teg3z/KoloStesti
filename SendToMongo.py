52c350f35adfef51bbe5ee615a87584d18c78c0e
Teg3z <tegez623@gmail.com>
2023-06-24 20:30:40 +0200

    Logs moved to mongoDB
    Co-authored-by: R4tmax <R4tmax@users.noreply.github.com>

---
+++ SendToMongo.py
@@ -0,0 +1,74 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import re
+
+CESTA_LOGY = r"C:\Users\Sviha\Desktop\apps\Programming\KoloStesti\KoloStesti\\"
+
+CESTA_LOGY_DK = CESTA_LOGY + "LogsDK.txt"
+CESTA_LOGY_DF = CESTA_LOGY + "LogsDF.txt"
+CESTA_LOGY_DFK = CESTA_LOGY + "LogsDFK.txt"
+CESTA_LOGY_D = CESTA_LOGY + "LogsD.txt"
+CESTA_LOGY_DFKM = CESTA_LOGY + "LogsDFKM.txt"
+CESTA_LOGY_DKKA = CESTA_LOGY + "LogsDKKA.txt"
+
+list_logu = [CESTA_LOGY_DF, CESTA_LOGY_DFK, CESTA_LOGY_DFKM, CESTA_LOGY_DK, CESTA_LOGY_DKKA]
+
+# pripoj se k DB
+# databaze
+uri = "mongodb+srv://WOFadmin:TRqz3dHcnAbQataY@wheelofluck.tqghhhz.mongodb.net/?retryWrites=true&w=majority"
+# # Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# connect to database namespace
+db = client['WheelOfLuck']
+
+def retrieveGameDataFromLine(line):
+        document_entries = line.strip().split(' ')
+        game_date = document_entries[0]
+        game_result = document_entries[len(document_entries) - 1]
+        document_entries.pop()
+        document_entries.pop(0)
+        game_name = ""
+        for entry in document_entries:
+            game_name += entry + " "
+        game_name.strip()
+
+        post = {
+            "game_date": game_date,
+            "game": game_name,
+            "result": game_result
+        }
+
+        return post
+
+def getCollection(path):
+    match = re.search(r"(Logs.+?)\.txt", path)
+
+    if match:
+         captureGroup = match.group(1)    
+              
+    return captureGroup
+    
+
+def sendToMongoDB(json_data,collection):
+    collection.insert_one(json_data)
+
+def main():
+    #try:
+    for log in list_logu:
+        soubor = open(log, "rt")
+        line = soubor.readline()
+        while line:
+            # ziskej data z line
+            json_data = retrieveGameDataFromLine(line)
+            #collection = $najdi kollekci podle Logu
+            collection = db[getCollection(log)]
+            sendToMongoDB(json_data, collection)
+            line = soubor.readline()
+        soubor.close()
+    #except Exception as chybaNevimPicoVcem:
+    #    print("Chyba: " + str(chybaNevimPicoVcem))
+    #    exit()
+
+
+if __name__ == "__main__":
+    main()
