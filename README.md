e72599d09dbf547b324f956a4326c7be61bb159b
Teg3z <tegez623@gmail.com>
2024-08-18 11:27:52 +0200

    Make code in english and Python standards

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# KoloStesti
-Nase vlastni kolo stesti
+# Wheel Of Luck
+Can't decide on which game to play with your friends tonight? Don't worry, we've got you covered!
+
+Spin the Wheel Of Luck that randomly chooses the game for you. Make your own list of games, that you consider playable tonight. The Wheel also let's your Discord server know what you are going to play! Maybe others will join your epic gaming experience?
+
+An epic project of enthusiastic IT students learning basic Python coding. MIT license of course.
+
+@Tegez, Jouker and @R4tmax
--- send_to_db.py
+++ send_to_db.py
@@ -2,11 +2,11 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import re
 
-CESTA_LOGY = r"C:\Users\Sviha\Desktop\apps\Programming\KoloStesti\KoloStesti\\"
+LOGS_PATH = r"C:\Users\Sviha\Desktop\apps\Programming\KoloStesti\KoloStesti\\"
 
-CESTA_RACE = CESTA_LOGY + "GTARacy.txt"
+GTA_LOGS_PATH = LOGS_PATH + "GTARacy.txt"
 
-list_logu = [CESTA_RACE]
+logs = [GTA_LOGS_PATH]
 
 # pripoj se k DB
 # databaze
@@ -16,44 +16,35 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 # connect to database namespace
 db = client['WheelOfLuck']
 
-
 def retrieveGameDataFromLine(line):
-    post = {
-        "race_name": line,
-    }
-    return post
-
+        post = {
+            "race_name": line,
+        }
+        return post
 
 def getCollection(path):
     match = re.search(r"(Logs.+?)\.txt", path)
-
     if match:
-        captureGroup = match.group(1)
-
+         captureGroup = match.group(1)    
     return captureGroup
 
-
-def sendToMongoDB(json_data, collection):
+def sent_to_db(json_data, collection):
     collection.insert_one(json_data)
 
-
 def main():
-    # try:
-    for log in list_logu:
-        soubor = open(log, "rt")
-        line = soubor.readline()
+    #try:
+    for log in logs:
+        file = open(log, "rt")
+        line = file.readline()
         while line:
-            # ziskej data z line
             json_data = retrieveGameDataFromLine(line)
-            # collection = $najdi kollekci podle Logu
             collection = db['GTARaces']
-            sendToMongoDB(json_data, collection)
-            line = soubor.readline()
-        soubor.close()
-    # except Exception as chybaNevimPicoVcem:
+            sent_to_db(json_data, collection)
+            line = file.readline()
+        file.close()
+    #except Exception as chybaNevimPicoVcem:
     #    print("Chyba: " + str(chybaNevimPicoVcem))
     #    exit()
-
 
 if __name__ == "__main__":
     main()
