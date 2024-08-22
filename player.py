334cb077104ff578fe6992e48abfc1456d826050
Teg3z <tegez623@gmail.com>
2024-08-22 17:45:38 +0200

    Add games conditions

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 class Player:
-    def __init__(self, name, discord_id, games):
+    def __init__(self, name, discord_id, games, games_conditions):
         self.name = name
         self.discord_id = discord_id
         self.games = games
+        self.games_conditions = games_conditions
