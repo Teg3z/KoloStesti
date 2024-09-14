0ac4af2d19ee84e9747cfbebded2284c1b49b9a3
R4tmax <kadlec.m.90@gmail.com>
2024-09-14 15:43:24 +0200

    Get cxfreeze to at least launch

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from cx_Freeze import setup, Executable
+import sys
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': [], 'excludes': []}
+build_options = {'includes': [],
+                 'excludes': [],
+                 'include_files': [('variables.env', 'variables.env')]
+                 }
 
-base = 'gui'
+base = "gui"
 
 executables = [
     Executable('wheel_of_luck.py', base=base)
 ]
 
 setup(name='WheelOfLuck',
-      version = '2.0.0',
+      version = '2.0.1',
       description = 'Interactive handler for Discord based solution of a game to play in a group',
       options = {'build_exe': build_options},
       executables = executables)
