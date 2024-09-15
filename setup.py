1331487ef727f6ee6935535e0a0e5685be5c9a30
R4tmax <kadlec.m.90@gmail.com>
2024-09-15 22:40:19 +0200

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -17,10 +17,6 @@ setup(name='WheelOfLuck',
       version = '2.0.1',
       description = 'Interactive handler for Discord based solution of a game to play in a group',
       options = {
-          'build_exe': build_options,
-          'bdist_rpm': {
-              'release': '1',  # Control the release number here
-              'packager': 'Martin @R4tmax Kadlec kadlec.m.90@gmail.com',
-          }
-      },
+          'build_exe': build_options
+     },
       executables = executables)
