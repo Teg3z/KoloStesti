47a97aca299eaebea5cb42f03aa866239156df57
R4tmax <kadlec.m.90@gmail.com>
2024-09-15 22:35:30 +0200

    Attempt to add specific instructions for RPM builds to correctly work with release numbers

--- setup.py
+++ setup.py
@@ -16,5 +16,11 @@ executables = [
 setup(name='WheelOfLuck',
       version = '2.0.1',
       description = 'Interactive handler for Discord based solution of a game to play in a group',
-      options = {'build_exe': build_options},
+      options = {
+          'build_exe': build_options,
+          'bdist_rpm': {
+              'release': '1',  # Control the release number here
+              'packager': 'Martin @R4tmax Kadlec kadlec.m.90@gmail.com',
+          }
+      },
       executables = executables)
